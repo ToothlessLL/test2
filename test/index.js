6db85763ed4a00efb5d5163f30c5f65d59f1714c
ToothlessLL <128851832+ToothlessLL@users.noreply.github.com>
2025-07-07 22:17:33 -0700

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,2 +1,4 @@
-fetch('http://frostnibblebot:8080/files/config.json')
+fetch('http://frostnibblebot.railway.internal:8080/files/config.json')
+.then(result => console.log(result));
+fetch('http://frostnibblebot.railway.internal:8080/api/public/config.json')
 .then(result => console.log(result));
