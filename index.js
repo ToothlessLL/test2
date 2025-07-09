75d960faff0d856528fac47668b44abdd444a44d
alan <rubikscubesrs@gmail.com>
2025-07-08 22:58:47 -0700

    test updated path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import postgres from 'postgres';
 import fs from 'fs';
 
-const path = `./data/output.txt`;
+const path = `/data/output.txt`;
 const sql = postgres(
     process.env.DATABASE_URL
     , {
@@ -15,20 +15,20 @@ const sql = postgres(
 const data = await sql`select * from cluestats`;
 
 // fs.writeFile(`${process.env.RAILWAY_VOLUME_MOUNT_PATH}/output.txt`, JSON.stringify(data), 'utf8', (err) => {
-// fs.writeFile(toNamespacedPath, JSON.stringify(data), 'utf8', (err) => {
-//     if (err) {
-//         console.error('Error writing file: ', err);
-//         return;
-//     }
-//     console.log('File written succesfully');
-// })
-
-fs.readFile(path, 'utf8', (err, data) => {
+fs.writeFile(path, JSON.stringify(data), 'utf8', (err) => {
     if (err) {
-        console.error('Error reading file: ', err);
+        console.error('Error writing file: ', err);
         return;
     }
-    console.log(data);
+    console.log('File written succesfully');
 });
+
+// fs.readFile(path, 'utf8', (err, data) => {
+//     if (err) {
+//         console.error('Error reading file: ', err);
+//         return;
+//     }
+//     console.log(data);
+// });
 
 console.log(data);
